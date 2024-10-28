0030ff077d32d0452ee148b445a8d232631bd13e
Ilia Kulakov <veterkam@gmail.com>
2024-10-28 20:39:48 +0100

    Init part2app

--- chapter-08/webapp/dist/readHandler.js
+++ chapter-08/webapp/dist/readHandler.js
@@ -1,13 +1,12 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
 exports.readHandler = void 0;
-const promises_1 = require("fs/promises");
+const promises_1 = require('fs/promises');
 const readHandler = async (req, res) => {
     try {
         res.setHeader('Content-Type', 'application/json');
         res.write(await (0, promises_1.readFile)('data.json'));
-    }
-    catch (err) {
+    } catch (err) {
         res.writeHead(500);
     }
     res.end();
