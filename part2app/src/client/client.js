2741d3976b7536ccea4b0514dfe3def940f28c9c
Ilia Kulakov <veterkam@gmail.com>
2024-11-14 08:22:21 +0100

    Client templates

--- part2app/src/client/client.js
+++ part2app/src/client/client.js
@@ -1,15 +1,26 @@
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('btn').addEventListener('click', sendRequest);
-});
+import { Counter } from './counter_custom';
 
-sendRequest = async () => {
-    const response = await fetch('/test', {
-        method: 'POST',
-        body: JSON.stringify({ message: 'Hello World!' }),
-        headers: { 'Content-Type': 'application/json' },
-    });
-    document.getElementById('msg').textContent = response.statusText;
-    let text = await response.text();
-    document.getElementById('body').innerHTML = text;
-    return text;
+const context = {
+    counter: 0,
 };
+
+const actions = {
+    incrementCounter: () => {
+        context.counter++;
+        render();
+    },
+};
+
+const render = () => {
+    document.getElementById('target').innerHTML = Counter(context);
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.onclick = (ev) => {
+        const action = ev.target.getAttribute('action');
+        if (action && actions[action]) {
+            actions[action]();
+        }
+    };
+    render();
+});
