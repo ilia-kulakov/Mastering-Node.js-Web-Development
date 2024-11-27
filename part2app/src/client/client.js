293a53fe0fb46bc1c6bf2235756e0722afcb9b37
Ilia Kulakov <veterkam@gmail.com>
2024-11-27 20:38:46 +0100

    part2app Client-side validation

--- part2app/src/client/client.js
+++ part2app/src/client/client.js
@@ -1,3 +1,28 @@
+import { validate } from './client_validation';
+
 document.addEventListener('DOMContentLoaded', () => {
-    //
+    document.getElementById('age_form').onsubmit = (event) => {
+        const data = new FormData(event.target);
+
+        const nameValid = validate('name', data).required().minLength(5);
+
+        const ageValid = validate('age', data).isInteger();
+
+        const allValid = [nameValid, ageValid]
+            .flatMap((validation) =>
+                Object.entries(validation.results).map(([test, valid]) => {
+                    const e = document.getElementById(
+                        `err_${validation.propertyName}_${test}`
+                    );
+                    e.classList.add('bg-dark-subtle');
+                    e.style.display = valid ? 'none' : 'block';
+                    return valid;
+                })
+            )
+            .every((valid) => valid === true);
+
+        if (!allValid) {
+            event.preventDefault();
+        }
+    };
 });
