c4d88e8a871961b76cc9b77fa6d2db3311e9b9b1
Ilia Kulakov <veterkam@gmail.com>
2024-12-28 18:45:12 +0100

    Chapter-11: Init

---
+++ chapter-11/src/server/server.ts
@@ -0,0 +1,43 @@
+import { createServer } from 'http';
+import express, { Express } from 'express';
+import httpProxy from 'http-proxy';
+import helmet from 'helmet';
+import { testHandler } from './testHandler';
+import { engine } from 'express-handlebars';
+import * as helpers from './template_helpers';
+
+const port = 5000;
+
+const expressApp: Express = express();
+
+const proxy = httpProxy.createProxyServer({
+    target: 'http://localhost:5100',
+    ws: true,
+});
+
+expressApp.set('views', 'templates/server');
+
+expressApp.engine('handlebars', engine());
+expressApp.set('view engine', 'handlebars');
+
+expressApp.use(helmet());
+expressApp.use(express.json());
+
+expressApp.get('/dynamic/:file', (req, res) => {
+    res.render(`${req.params.file}.handlebars`, {
+        message: 'Hello template',
+        req,
+        helpers: { ...helpers },
+    });
+});
+
+expressApp.post('/test', testHandler);
+expressApp.use(express.static('static'));
+expressApp.use(express.static('node_modules/bootstrap/dist'));
+expressApp.use((req, res) => proxy.web(req, res));
+
+const server = createServer(expressApp);
+
+server.on('upgrade', (req, socket, head) => proxy.ws(req, socket, head));
+
+server.listen(port, () => console.log(`HTTP Server listening on port ${port}`));
