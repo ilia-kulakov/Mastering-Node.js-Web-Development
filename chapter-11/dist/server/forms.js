b3694c9ab8c19094812ee7d03fcb78e74c6da1a7
Ilia Kulakov <veterkam@gmail.com>
2024-12-29 13:12:29 +0100

    Chapter-11: Form's data processing

--- chapter-11/dist/server/forms.js
+++ chapter-11/dist/server/forms.js
@@ -1,11 +1,27 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.registerFormRoutes = exports.registerFormMiddleware = void 0;
+const express_1 = __importDefault(require("express"));
 const registerFormMiddleware = (app) => {
-    // no middleware
+    app.use(express_1.default.urlencoded({ extended: true }));
 };
 exports.registerFormMiddleware = registerFormMiddleware;
 const registerFormRoutes = (app) => {
-    // no routes yet
+    app.get('/form', (req, res) => {
+        for (const key in req.query) {
+            res.write(`${key}: ${req.query[key]}\n`);
+        }
+        res.end();
+    });
+    app.post('/form', (req, res) => {
+        res.write(`Content-Type: ${req.headers['content-type']}\n`);
+        for (const key in req.body) {
+            res.write(`${key}: ${req.body[key]}\n`);
+        }
+        res.end();
+    });
 };
 exports.registerFormRoutes = registerFormRoutes;
--- chapter-11/src/server/forms.ts
+++ chapter-11/src/server/forms.ts
@@ -1,9 +1,22 @@
-import { Express } from 'express';
+import express, { Express } from 'express';
 
 export const registerFormMiddleware = (app: Express) => {
-    // no middleware
+    app.use(express.urlencoded({ extended: true }));
 };
 
 export const registerFormRoutes = (app: Express) => {
-    // no routes yet
+    app.get('/form', (req, res) => {
+        for (const key in req.query) {
+            res.write(`${key}: ${req.query[key]}\n`);
+        }
+        res.end();
+    });
+
+    app.post('/form', (req, res) => {
+        res.write(`Content-Type: ${req.headers['content-type']}\n`);
+        for (const key in req.body) {
+            res.write(`${key}: ${req.body[key]}\n`);
+        }
+        res.end();
+    });
 };
