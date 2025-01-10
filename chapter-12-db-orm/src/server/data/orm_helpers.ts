bee1f28772e6005502e18feb32aad6cce45ac3ca
Ilia Kulakov <veterkam@gmail.com>
2025-01-10 18:45:34 +0100

    Chapter-12: Database ORM.

--- chapter-12-db-orm/src/server/data/orm_helpers.ts
+++ chapter-12-db-orm/src/server/data/orm_helpers.ts
@@ -1,5 +1,6 @@
 import { DataTypes, Sequelize } from 'sequelize';
 import { Calculation, Person, ResultModel } from './orm_models';
+import { Result } from './repository';
 
 const primaryKey = {
     id: {
@@ -34,4 +35,38 @@ export const initializeModels = (sequelize: Sequelize) => {
         },
         { sequelize }
     );
+};
+
+export const defineRelationships = () => {
+    ResultModel.belongsTo(Person, { foreignKey: 'personId' });
+    ResultModel.belongsTo(Calculation, { foreignKey: 'calculationId' });
+};
+
+export const addSeedData = async (sequelize: Sequelize) => {
+    await sequelize.query(`
+        INSERT INTO Calculations (id, age, years, nextage, createdAt, updatedAt) VALUES
+            (1, 35, 5, 40, date(), date()),
+            (2, 35, 10, 45, date(), date())
+        `);
+    await sequelize.query(`
+        INSERT INTO People (id, name, createdAt, updatedAt) VALUES
+            (1, 'Alice', date(), date()),
+            (2, 'Bob', date(), date())
+        `);
+    await sequelize.query(`
+        INSERT INTO ResultModels (calculationId, personId, createdAt, updatedAt) VALUES
+            (1, 1, date(), date()),
+            (2, 2, date(), date()),
+            (2, 1, date(), date())
+        `);
+};
+
+export const fromOrmModel = (model: ResultModel | null): Result => {
+    return {
+        id: model?.id ?? 0,
+        name: model?.Person?.name ?? '',
+        age: model?.Calculation?.age ?? 0,
+        years: model?.Calculation?.years ?? 0,
+        nextage: model?.Calculation?.nextage ?? 0,
+    };
 };
